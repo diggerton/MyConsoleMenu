b025b1aeda9c3f074cd4c99fbcafec8cb91d35c5
Josh Tomlinson <ycland@gmail.com>
2017-04-19 11:47:01 -0700

    accidentally deleted the utilities project from the old combined project (before splitting) - yeah, confusing

--- MyConsoleMenu/ConsoleMenu.cs
+++ MyConsoleMenu/ConsoleMenu.cs
@@ -1,10 +1,7 @@
 ﻿using MyConsoleMenu.Models;
-using MySimpleUtilities.ConsoleExtended;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyConsoleMenu
 {
